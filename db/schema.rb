e2d3076084a1db507b4d496ceb063b7eecd4ac31
Sota <sooota09@gmail.com>
2020-05-23 23:16:55 +0900

    [delete] storesテーブルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_053715) do
-
-  create_table "stores", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2020_05_23_141521) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
