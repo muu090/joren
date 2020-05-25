8640e1ae55ecf692c7d5af80a4ad18c37a39c185
Sota <sooota09@gmail.com>
2020-05-25 18:07:54 +0900

    [update] ログイン前のアクセス制限設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   #sign_in後のredirect先をマイページ(ユーザ詳細ページ)へ
--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,6 @@
 class StoresController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
   end
 end
