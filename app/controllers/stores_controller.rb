61cd28de657192744b1d5931df98cf3e21032d48
Sota <sooota09@gmail.com>
2020-06-01 21:17:45 +0900

    [update] 検索画面表示修正

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,6 +1,25 @@
+require 'json'
+require 'net/http'
+require "uri"
 class StoresController < ApplicationController
   before_action :authenticate_user!
-  
+
+  def index
+    @keyword = params[:keyword]
+
+    data = {
+      "key": ENV['API_KEY'],
+      "keyword": @keyword,
+      "format": "json"
+    }
+    
+    query = data.to_query
+    uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?" + query)
+    res = Net::HTTP.get_response(uri)
+    res_data = JSON.parse(res.body)
+    @shops = res_data["results"]["shop"]
+  end
+    
   def show
   end
 end
