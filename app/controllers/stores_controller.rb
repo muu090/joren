62eebca6fbbcd728d998595f4eab22b14ec46185
Sota <sooota09@gmail.com>
2020-06-02 21:54:09 +0900

    [finish] 店舗検索/検索結果表示・店舗詳細画面表示機能を実装

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -5,7 +5,7 @@ class StoresController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @keyword = params[:keyword]
+    @keyword = params[:keyword]  # ユーザが入力した検索キーワードをパラメータから取得
 
     data = {
       "key": ENV['API_KEY'],
@@ -18,12 +18,43 @@ class StoresController < ApplicationController
     res = Net::HTTP.get_response(uri)
     res_data = JSON.parse(res.body)
 
-    results = res_data["results"]["shop"]
+    results = res_data["results"]["shop"]  # 検索結果のjsonデータ(配列)をまるっと格納
+
     if results
-      @shops = Kaminari.paginate_array(results).page(params[:page]).per(5)
+      @shops = Kaminari.paginate_array(results).page(params[:page]).per(5)  # 検索結果を1ページ当たり5件まで表示
+      
+      shop_ids = results.map { |shop| shop["id"] } # 検索結果の中から、(API上の)idのみを配列で取り出す
+      shop_ids.each do |shop_id|
+        stores = Store.all
+        store = stores.find_or_initialize_by(api_id: shop_id) # 検索結果から取り出したapi_idを元にStoreモデルを参照
+        
+        if store.new_record? # 新規データだった場合は保存する
+          store = Store.new  
+          store.api_id = shop_id
+          store.save! 
+        end
+
+        # @store = Store.find_by(api_id: shop_id)
+      end
     end
   end
-    
+  
   def show
+
+    store = Store.find(params[:id]) # API上の店舗idを持ったモデル情報をパラメータのidを元に取得
+    id = store.api_id  # 取得したモデル情報のapi_id(API上の店舗id)をidに代入
+    
+    data = {
+      "key": ENV['API_KEY'],
+      "id": id,
+      "format": "json"
+    }
+    
+    query = data.to_query
+    uri = URI.parse("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?" + query)
+    res = Net::HTTP.get_response(uri)
+    res_data = JSON.parse(res.body)
+
+    @shop = res_data["results"]["shop"]  # 検索結果のjsonデータ(配列)をまるっと格納
   end
 end
