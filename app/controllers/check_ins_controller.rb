2f87f524dff83384618f913652bb708b032bd77a
Sota <sooota09@gmail.com>
2020-06-08 23:24:13 +0900

    [fix] チェックイン履歴のコメント表示を修正

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -9,6 +9,9 @@ class CheckInsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @check_ins = CheckIn.where(user_id: @user.id, store_id: params[:store_id]).order(created_at: :desc) # 最新順に表示
+    @check_ins.each do |check_in|
+      @comments = Comment.where(check_in_id: check_in.id)
+    end
   end
 
   def create
