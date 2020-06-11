0ad0432dfa8233ab79761758eb68a821f32b62f5
Sota <sooota09@gmail.com>
2020-06-11 20:06:29 +0900

    [update] url直打ち防止

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def barrier_user(user)
+    unless user == current_user or current_user.following?(user)
+      redirect_to user_path(current_user)
+    end
+  end
+
   #sign_in後のredirect先をマイページ(ユーザ詳細ページ)へ
   def after_sign_in_path_for(resource)
     user_path(current_user.id)
